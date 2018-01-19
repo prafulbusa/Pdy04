1185ced77a1afba82b98eea2dfa9cd10fd683257
Praful Busa <busap@uw.edu>
2018-01-19 01:42:00 -0800

    Connection Resiliency and log SQL commands that Entity Framework Composes and sends to the Database.

---
+++ ContosoUniversity/DAL/SchoolConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using System.Data.Entity;
+using System.Data.Entity.Infrastructure.Interception;
+using System.Data.Entity.SqlServer;
+namespace ContosoUniversity.DAL
+{
+    public class SchoolConfiguration : DbConfiguration
+    {
+        public SchoolConfiguration()
+        {
+            SetExecutionStrategy("System.Data.SqlClient", () => new
+            SqlAzureExecutionStrategy());
+            // SetDefaultConnectionFactory(new LocalDbConnectionFactory("v11.0"));
+            DbInterception.Add(new SchoolInterceptorTransientErrors());
+            DbInterception.Add(new SchoolInterceptorLogging());
+        }
+    }
+}
